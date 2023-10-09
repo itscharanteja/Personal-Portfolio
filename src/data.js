1a43bc62d4e196fefb58e082e0b452a2ede01394
Charan Teja <68527259+itscharanteja@users.noreply.github.com>
2023-10-09 22:43:26 +0530

    Final react app

---
+++ src/data.js
@@ -0,0 +1,19 @@
+export const featuredPortfolio = [
+  { id: 1, title: "Blood Bank", img: "assets/project-blood.png" },
+  { id: 2, title: "User To-Do List", img: "assets/project-blood.png" },
+  { id: 3, title: "StopWatch App", img: "assets/project-stop.png" },
+  { id: 4, title: "Tic-Tac-Toe", img: "assets/project-tic.png" },
+];
+
+export const webPortfolio = [
+  { id: 1, title: "Blood Bank", img: "assets/project-blood.png" },
+  { id: 2, title: "User To-Do List", img: "assets/project-blood.png" },
+];
+
+export const mobilePortfolio = [
+  { id: 1, title: "StopWatch App", img: "assets/project-stop.png" },
+];
+
+export const gamesPortfolio = [
+  { id: 1, title: "Tic-Tac-Toe", img: "assets/project-tic.png" },
+];
